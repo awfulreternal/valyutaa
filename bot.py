10d8dbf75411560fdc6313019d6048d60b38d785
awfulreternal <matveykudelin7@gmail.com>
2024-07-12 00:26:20 +0300

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-# bot version: 1.3
-
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import config as cfg
-
-
-bot = Bot(cfg.7294993191:AAEFCphfC1PraUbSkD8IyWvTEg6qGGyvAtQ, parse_mode='HTML')
-dp = Dispatcher(bot, storage=MemoryStorage())
